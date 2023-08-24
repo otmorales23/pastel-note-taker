d81a43026bad841a50c7e553984c86aa1d343904
otmorales23 <oliverwills0227@gmail.com>
2023-08-23 23:59:31 -0700

    added function to save notes to db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,8 +4,26 @@ const notes = require("../db/db.json")
 const express = require("express")
 const router = express.Router()
 
-
+// let notes = notesData
 
 const saveNotes = () => {
-    fs.writeFile(path.resolves(__dirname, "../db/db.json"), notes)
+    fs.writeFileSync(path.resolve(__dirname, "../db/db.json"), JSON.stringify(notes))
 }
+
+
+router.get("/notes", (req, res) => {
+    console.log("Hello");
+    res.json(notes)
+})
+
+router.post("/notes", (req, res) => {
+    notes.push(req.body)
+    saveNotes(notes)
+    res.json(notes)
+})
+
+router.delete("/notes/:id", (req, res) => {
+    res.json(notes)
+})
+
+module.exports = router;
